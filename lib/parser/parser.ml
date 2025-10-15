b34154bd69b45d9be4b8fb9bb8cf15b109d5d587
Eric Lee <me@ericchanlee.com>
2025-10-14 17:49:50 -0700

    Tag parser combinators with line / col

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -1,6 +1,13 @@
 open Core
 
-type 'a t = char Sequence.t -> ('a * char Sequence.t) Or_error.t
+type pos =
+  { line : int
+  ; col : int
+  }
+[@@deriving sexp]
+
+type stream = (char * pos) Sequence.t
+type 'a t = stream -> ('a * stream) Or_error.t
 
 include Applicative.Make (struct
     type nonrec 'a t = 'a t
@@ -55,49 +62,73 @@ end
 open Let_syntax
 open Infix_syntax
 
-let fail : 'a t = fun _ -> error_s [%message "fail"]
+let fail ~pos msg = error_s [%message (pos : pos) msg]
 
 let satisfy (pred : char -> bool) : char t =
   fun st ->
   match Sequence.next st with
-  | Some (c, st') when pred c -> Ok (c, st')
-  | Some _ -> Or_error.error_string "satisfy fail invalid"
-  | None -> Or_error.error_string "satisfy fail EOF"
+  | Some ((c, pos), st') ->
+    if pred c then Ok (c, st') else fail ~pos "satisfy: pred not satisfied"
+  | None -> Or_error.error_string "satisfy: EOF"
 ;;
 
 let rec many1 p = List.cons <$> p <*>| lazy (many p)
 and many p = many1 p <|> return []
 
-let choice = List.fold_right ~f:( <|> ) ~init:fail
-let sepBy1 sep p = List.cons <$> p <*> many (sep *> p)
-let sepBy sep p = sepBy1 sep p <|> return []
-let charP c = satisfy (Char.equal c)
-let stringP st = all (List.map ~f:charP (String.to_list st))
-let alphaP = many1 (satisfy Char.is_alpha)
-let numericP = many1 (satisfy Char.is_digit)
-let emptyP = charP ' ' <|> charP '\n' <|> charP '\t'
-let strip p = ignore_m (many emptyP) *> p <* ignore_m (many emptyP)
-let spacesP = many1 emptyP
+let sep_by1 sep p = List.cons <$> p <*> many (sep *> p)
+let sep_by sep p = sep_by1 sep p <|> return []
+let char_p c = satisfy (Char.equal c)
+let string_p st = all (List.map ~f:char_p (String.to_list st))
+let alpha_p = many1 (satisfy Char.is_alpha)
+let numeric_p = many1 (satisfy Char.is_digit)
+let empty_p = char_p ' ' <|> char_p '\n' <|> char_p '\t'
+let strip p = ignore_m (many empty_p) *> p <* ignore_m (many empty_p)
+let spaces_p = many1 empty_p
 
-let%expect_test "check" =
-  let integerP =
+let stream_of_string s =
+  Sequence.unfold_step
+    ~init:(`Chars (String.to_list s, { line = 1; col = 1 }))
+    ~f:(function
+      | `Done -> Done
+      | `Chars ([], _) -> Done
+      | `Chars (c :: cs, pos) ->
+        let pos' =
+          if Char.equal c '\n'
+          then { line = pos.line + 1; col = 1 }
+          else { line = pos.line; col = pos.col + 1 }
+        in
+        Yield { value = c, pos; state = `Chars (cs, pos') })
+;;
+
+let run p s =
+  let%bind.Or_error x, st = p (stream_of_string s) in
+  match Sequence.next st with
+  | Some ((_, pos), _) -> fail ~pos "run: stream not fully consumed"
+  | None -> Ok x
+;;
+
+let%expect_test "tests" =
+  let integer_p =
     let%bind num =
-      numericP
-      <|> (List.cons <$> charP '-' <*> numericP)
-      <|> (List.cons <$> charP '+' <*> numericP)
+      numeric_p
+      <|> (List.cons <$> char_p '-' <*> numeric_p)
+      <|> (List.cons <$> char_p '+' <*> numeric_p)
     in
     return (int_of_string (String.of_list num))
   in
-  let dosP =
-    let%bind n = integerP in
-    let%bind _ = spacesP in
-    let%bind m = integerP in
+  let two_nums_p =
+    let%bind n = integer_p in
+    let%bind _ = spaces_p in
+    let%bind m = integer_p in
     return (n, m)
   in
-  let x = dosP (String.to_sequence "12 -31") in
-  print_s [%message (x : ((int * int) * char Sequence.t) Or_error.t)];
-  [%expect {| test |}];
-  let x = stringP "abcde" (String.to_sequence "abcde") in
-  print_s [%message (x : (char list * char Sequence.t) Or_error.t)];
-  [%expect {| test |}]
+  let x = run two_nums_p "12 -31" in
+  print_s [%message (x : (int * int) Or_error.t)];
+  [%expect {| (x (Ok (12 -31))) |}];
+  let x = run two_nums_p "12 -a1" in
+  print_s [%message (x : (int * int) Or_error.t)];
+  [%expect {| (x (Error ((pos ((line 1) (col 4))) "satisfy: pred not satisfied"))) |}];
+  let x = run (string_p "abcde") "abcde  " in
+  print_s [%message (x : char list Or_error.t)];
+  [%expect {| (x (Error ((pos ((line 1) (col 6))) "run: stream not fully consumed"))) |}]
 ;;
