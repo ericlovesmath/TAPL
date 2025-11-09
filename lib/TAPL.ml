1c0c6577b24119a3949e99d9549f9711202a286d
Eric Lee <me@ericchanlee.com>
2025-11-08 23:19:42 -0800

    Copy code to prepare for featherweight_java

--- lib/TAPL.ml
+++ lib/TAPL.ml
@@ -3,4 +3,5 @@ module Untyped_nameless = Untyped_nameless
 module Simply_typed_lambda_calculus = Simply_typed_lambda_calculus
 module Simply_typed_extended = Simply_typed_extended
 module Subtyping = Subtyping
+module Featherweight_java = Featherweight_java
 module Chomp = Chomp
---
+++ lib/featherweight_java/featherweight_java.mli
@@ -0,0 +1,4 @@
+module Types = Types
+
+(* Reads and parses [string] input, prints result *)
+val repl : string -> unit
